bb9350cfe9abbe729d7b99333cfe2e156fa9a398
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-05-31 16:32:22 +0800

    complete this app

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :memberships
+  has_many :groups, :through => :memberships
   def display_name
     self.email.split("@").first
   end
