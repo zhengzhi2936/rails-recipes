e809ad286721ec1f9e57f731cf17e8e1b3bb1b9d
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-06-01 10:05:48 +0800

    add profile to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :find_user
   def edit
-    @user = current_user
   end
 
+  def show
+  end
   def update
-    @user = current_user
     if @user.update(user_params)
       flash[:notice] = "修改成功"
       redirect_to edit_user_path
@@ -17,7 +17,11 @@ class UsersController < ApplicationController
 
   protected
 
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  has_one :profile
   has_many :memberships
   has_many :groups, :through => :memberships
+
+  accepts_nested_attributes_for :profile
   def display_name
     self.email.split("@").first
   end
